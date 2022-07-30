eb05f10176df547aae4ff242937d18e711e8437a
Jonathan <KuugJonathan45@gmail.com>
2022-07-30 14:27:32 +0000

    added icons

--- frontend/src/pages/videoPlayerPage/components/NextPrevControls.jsx
+++ frontend/src/pages/videoPlayerPage/components/NextPrevControls.jsx
@@ -1,11 +1,12 @@
+import {GoChevronLeft, GoChevronRight} from 'react-icons/go'
 const NextPrevControls = () => {
   return <div className="w-full h-5 my-6 flex justify-between">
     <div>
 
     </div>
-    <div>
-        <button>Prev</button>
-        <button>Next</button>
+    <div className='flex items-center'>
+        <button className="mr-9 font-light flex items-center"><GoChevronLeft/>Prev</button>
+        <button className="mr-9 font-light flex items-center">Next<GoChevronRight/></button>
     </div>
   </div>;
 };
--- frontend/src/pages/videoPlayerPage/components/ServerOptions.jsx
+++ frontend/src/pages/videoPlayerPage/components/ServerOptions.jsx
@@ -1,14 +1,43 @@
-
+import { MdClosedCaption } from "react-icons/md";
+import {FaMicrophoneAlt} from "react-icons/fa"
 const ServerOptions = () => {
   return (
     <div className="my-3 w-full h-28 flex text-left">
-        <div className="w-1/3 px-5 py-5 bg-[#2c2c2d] text-[#6d6d6d] text-sm flex items-center justify-center">If the current server doesn't work please try other streaming options</div>
-        <div className="w-full bg-[#232323]">
-            <div></div>
-            <div></div>
+      <div className="w-1/3 px-5 py-5 bg-[#2c2c2d] text-[#6d6d6d] text-sm flex items-center justify-center">
+        If the current server doesn't work please try other streaming options
+      </div>
+      <div className="w-full max-h-full px-8 py-6  bg-[#232323] flex flex-col items-start justify-center">
+        <div className="flex items-center mb-6">
+          <span className="mr-10 flex items-center text-[16px]">
+            <MdClosedCaption className="mr-2" />
+            <span>Sub :</span>
+          </span>
+          <div className="text-[#ccc]">
+            <button className="font-semibold text-sm px-3 py-2 mr-2 bg-[#343434] rounded-[3px]">
+              Vidcloud
+            </button>
+            <button className="font-semibold text-sm px-3 py-2 mr-2 bg-[#343434] rounded-[3px]">
+              StreamSB
+            </button>
+          </div>
         </div>
+        <div className="flex items-center">
+          <span className="mr-10 flex items-center text-[16px]">
+            <FaMicrophoneAlt className="mr-2" />
+            <span>Dub :</span>
+          </span>
+          <div className="text-[#ccc]">
+            <button className="font-semibold px-3 text-sm py-2 mr-2 bg-[#343434] rounded-[3px]">
+              Vidcloud
+            </button>
+            <button className="font-semibold px-3 text-sm py-2 mr-2 bg-[#343434] rounded-[3px]">
+              StreamSB
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServerOptions
+export default ServerOptions;
